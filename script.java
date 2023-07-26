f95d1f88d246a8b11cc3faf6d47ecd43d073d4e3
chamanos <chamano.sr@gmail.com>
2023-07-26 09:13:30 +0200

    first commit

---
+++ script.java
@@ -0,0 +1,13 @@
+/*Learning OOP Concepts in Java*/ 
+
++--------------------------------------+
+|                Person                |
++--------------------------------------+
+| - name: String                      |
+| - age: int                          |
++--------------------------------------+
+| + setName(name: String): void       |
+| + setAge(age: int): void            |
+| + getName(): String                 |
+| + getAge(): int                     |
++--------------------------------------+
