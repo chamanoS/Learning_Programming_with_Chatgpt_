2162fd3e8107d1bf751cbae642a0aee5041f5ed0
chamanos <chamano.sr@gmail.com>
2023-07-28 08:20:37 +0200

    polymorphism class and subclasses

---
+++ Animal.java
@@ -0,0 +1,30 @@
+class Animal {
+Animal animal = new Animal();
+
+    void makeSound(){
+
+    }
+}
+
+class Dog extends Animal {
+
+
+    @Override
+    void makeSound(){
+
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound(){
+        
+    }
+}
+
+class Bird extends Animal {
+    @Override
+    void makeSound(){
+        
+    }
+}
