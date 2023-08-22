478c4de85baaa662917745627778239406ef712b
chamanos <chamano.sr@gmail.com>
2023-08-22 14:50:44 +0200

    arithmatic

---
+++ Basic.java
@@ -0,0 +1,13 @@
+public class Basic {
+    public static void main(String[] args) {
+        //Practice 3
+        int length = 12;
+        int breath = 12;
+        int area = length*breath;
+        System.out.print(area);
+
+        //practice 4
+        
+    }
+    
+}
